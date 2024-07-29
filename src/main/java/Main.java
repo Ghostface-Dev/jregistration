33936d85f6e9bbf8bd36acb800c51adf457cef5e
Henrique Gustavo <henriquedev6@gmail.com>
2024-07-28 23:52:34 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        LocalData data = new LocalData("C:\\Users\\gkill\\Documents\\GitHub\\registro\\datas");
-
-        Entity entity = new Entity(34);
-
-        data.writeEntity(entity);
-        System.out.println(data.getEntity(34));
 
     }
 }
